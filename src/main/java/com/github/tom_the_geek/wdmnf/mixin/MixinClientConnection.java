e1817f0c36bed289e72c31bbc0255b31aac29547
Tom_The_Geek <tgbryan39@gmail.com>
2022-02-17 13:49:53 +0000

    Update to support the new snapshots

--- src/main/java/com/github/tom_the_geek/wdmnf/mixin/MixinClientConnection.java
+++ src/main/java/com/github/tom_the_geek/wdmnf/mixin/MixinClientConnection.java
@@ -3,7 +3,8 @@ package com.github.tom_the_geek.wdmnf.mixin;
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.PacketEncoderException;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,10 +18,10 @@ import java.util.concurrent.TimeoutException;
 public class MixinClientConnection {
     @Shadow @Final private static Logger LOGGER;
 
-    @Inject(method = "exceptionCaught", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;debug(Ljava/lang/String;Ljava/lang/Throwable;)V"))
+    @Inject(method = "exceptionCaught", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;debug(Ljava/lang/String;Ljava/lang/Throwable;)V"))
     private void on_exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable, CallbackInfo ci) {
         if (throwable instanceof PacketEncoderException) return;
         if (throwable instanceof TimeoutException) return;
-        LOGGER.fatal("Exception occurred in netty pipeline", throwable);
+        LOGGER.error("Exception occurred in netty pipeline", throwable);
     }
 }
